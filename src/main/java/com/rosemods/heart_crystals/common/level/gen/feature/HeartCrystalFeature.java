1f7550dbcb5ea8b8da4713042f1265632985a85d
rosevcook <rose.cook969@gmail.com>
2023-10-29 18:29:39 +0000

    air exposure

--- src/main/java/com/rosemods/heart_crystals/common/level/gen/feature/HeartCrystalFeature.java
+++ src/main/java/com/rosemods/heart_crystals/common/level/gen/feature/HeartCrystalFeature.java
@@ -4,6 +4,7 @@ import com.rosemods.heart_crystals.common.block.HeartCrystalBlock;
 import com.rosemods.heart_crystals.core.HCConfig;
 import com.rosemods.heart_crystals.core.registry.HCBlocks;
 import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.Feature;
@@ -20,18 +21,28 @@ public class HeartCrystalFeature extends Feature<NoneFeatureConfiguration> {
     public boolean place(FeaturePlaceContext<NoneFeatureConfiguration> context) {
         BlockPos origin = context.origin();
         WorldGenLevel level = context.level();
+        RandomSource rand = context.random();
         BlockState state = HCBlocks.HEART_CRYSTAL.get().defaultBlockState();
+        BlockPos desination = null;
+        int airAmount = 0;
 
-        for (int x = -4; x <= 4; ++x)
-            for (int z = -4; z <= 4; ++z)
-                for (int y = -4; y <= 4; ++y) {
+        for (int x = 0; x <= 6; ++x)
+            for (int z = 0; z <= 6; ++z)
+                for (int y = 0; y <= 6; ++y) {
                     BlockPos pos = origin.offset(x, y, z);
 
-                    if (pos.getY() < HCConfig.COMMON.maxYLevel.get() && (level.getBlockState(pos).getMaterial().isReplaceable() || level.isWaterAt(pos)) && state.canSurvive(level, pos)) {
-                        level.setBlock(pos, state.setValue(HeartCrystalBlock.WATERLOGGED, level.getFluidState(pos).is(Fluids.WATER)), 2);
-                        return true;
+                    if ((level.getBlockState(pos).getMaterial().isReplaceable() || level.isWaterAt(pos)) && !level.getFluidState(pos).is(Fluids.LAVA)) {
+                        airAmount++;
+
+                        if (pos.getY() < HCConfig.COMMON.maxYLevel.get() && state.canSurvive(level, pos))
+                            desination = pos;
                     }
                 }
+
+        if (desination != null && rand.nextInt(216) > airAmount) {
+            level.setBlock(desination, state.setValue(HeartCrystalBlock.WATERLOGGED, level.getFluidState(desination).is(Fluids.WATER)), 2);
+            return true;
+        }
 
         return false;
     }

5fcc80579a0552e786b79ba9afeb3722060b500f
rosevcook <rose.cook969@gmail.com>
2025-07-25 16:49:28 +0100

    cupids arrow logic tweaks

--- src/main/java/com/rosemods/heart_crystals/core/mixin/AbstractArrowMixin.java
+++ src/main/java/com/rosemods/heart_crystals/core/mixin/AbstractArrowMixin.java
@@ -8,31 +8,31 @@ import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(AbstractArrow.class)
 public class AbstractArrowMixin {
+    @Shadow
+    private int knockback;
+
     @Redirect(method = "onHitEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z"))
 
     private boolean onHitEntity(Entity instance, DamageSource source, float i) {
         AbstractArrow arrow = (AbstractArrow) (Object) this;
 
         if (arrow.getType() == HCEntityTypes.CUPIDS_ARROW.get() && instance instanceof LivingEntity entity) {
-            if (entity.isInvertedHealAndHarm()) {
-                DamageSource newSource = arrow.getOwner() == null ? entity.damageSources().magic() : entity.damageSources().indirectMagic(entity, arrow.getOwner());
-                return instance.hurt(newSource, i * 1.5f);
-            } else
-                entity.heal(6.0f);
+            Entity owner = arrow.getOwner();
 
-            if (entity instanceof Animal animal && !animal.isBaby()) {
-                Player player = null;
-                if (arrow.getOwner() instanceof Player pl)
-                    player = pl;
+            if (entity.isInvertedHealAndHarm())
+                return instance.hurt(owner == null ? entity.damageSources().magic()
+                        : entity.damageSources().indirectMagic(entity, owner), i * 1.5f);
+            else if (entity instanceof Animal animal && !animal.isBaby())
+                animal.setInLove(owner instanceof Player pl ? pl : null);
 
-                animal.setInLove(player);
-            }
-
+            entity.heal(6f);
+            this.knockback = 1;
             return true;
         }
 

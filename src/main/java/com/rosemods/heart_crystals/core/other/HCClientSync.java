6b8e5424fe7bea66e07a39974c7da59077e9a53c
Niklas Widmann <nik@somethingcatchy.net>
2025-08-04 09:46:30 +0200

    use context player

--- src/main/java/com/rosemods/heart_crystals/core/other/HCClientSync.java
+++ src/main/java/com/rosemods/heart_crystals/core/other/HCClientSync.java
@@ -1,20 +1,14 @@
 package com.rosemods.heart_crystals.core.other;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.entity.player.Player;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 
 public final class HCClientSync {
 
     public static void receivePacket(HCPlayerInfo.PlayerHealthInfoSync sync, IPayloadContext context) {
         context.enqueueWork(() -> {
-            Player player = Minecraft.getInstance().player;
-
-            if (player != null) {
-                HCPlayerInfo.PlayerHealthInfo info = HCPlayerInfo.getPlayerHealthInfo(player);
-                info.heartCount = sync.getHealthInfo().heartCount;
-                info.healthSet = sync.getHealthInfo().healthSet;
-            }
+            HCPlayerInfo.PlayerHealthInfo info = HCPlayerInfo.getPlayerHealthInfo(context.player());
+            info.heartCount = sync.getHealthInfo().heartCount;
+            info.healthSet = sync.getHealthInfo().healthSet;
         });
     }
 

ac713f3bfd0643b8262ab46c5948dfeb537d1b74
rosevcook <rose.cook969@gmail.com>
2025-09-06 23:18:47 +0100

    fix mixins

--- src/main/java/com/rosemods/heart_crystals/core/mixin/AbstractArrowMixin.java
+++ src/main/java/com/rosemods/heart_crystals/core/mixin/AbstractArrowMixin.java
@@ -8,19 +8,13 @@ import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(AbstractArrow.class)
 public class AbstractArrowMixin {
-    /*
-    @Shadow
-    private int knockback;
-
 
     @Redirect(method = "onHitEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z"))
-
     private boolean onHitEntity(Entity instance, DamageSource source, float i) {
         AbstractArrow arrow = (AbstractArrow) (Object) this;
 
@@ -34,11 +28,10 @@ public class AbstractArrowMixin {
                 animal.setInLove(owner instanceof Player pl ? pl : null);
 
             entity.heal(6f);
-            this.knockback = 1;
             return true;
         }
 
         return instance.hurt(source, i);
     }
-*/
+
 }
